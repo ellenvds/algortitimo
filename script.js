32f1dd65810aefa5e62d903a087100a2750d992d
ellenvds <00001108548453sp@al.educacao.sp.gov.br>
2024-08-27 18:55:15 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.btn-proximo');
+avanca.forEach(button=> {
+    button.addEventListener('click',function(){
+const atual = document.querySelector('.ativo'); 
+const ProximoPasso = 'passo' + this.getAtribute('data-proximo');
+
+atual.classList.remove('ativo'); 
+document.getElementById(proximoPasso).classList.add('ativo');
+})
+})
